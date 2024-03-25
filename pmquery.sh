7c1f3062572324d0ee9cf40f7f02345dc4083fae
xyc1719 <38127251+xyc1719@users.noreply.github.com>
2024-03-25 19:40:19 +0800

    update lab1

--- pmquery.sh
+++ pmquery.sh
@@ -1,4 +1,4 @@
-rm beijng.html* -rf 
+rm beijing.html* -rf 
 wget http://www.86pm25.com/city/beijing.html 
-grep "^<tr><td>" beijing.html | sed 's/<[^>]*>/ /g' | sed 's/\([0-9][0-9]*\)[^ ]* /\1/g' | awk '{print time "," $1 "," $3 }' time="$(date "+%Y-%m-%d %H:%M:%S")" | tee pmdata.csv
+grep "^<tr><td>" beijing.html | sed 's/<[^>]*>/ /g' | sed 's/\([0-9][0-9]*\)[^ ]* /\1/g' | awk -v time="$(date "+%Y-%m-%d %H:%M:%S")" '{print time "," $1 "," $3 }' | tee pmdata.csv
 
